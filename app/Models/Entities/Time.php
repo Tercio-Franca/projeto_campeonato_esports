6ede9d45a41e8780c8f346d65bae11020b185387
terciofranca <tercio.franca@hotmail.com>
2021-10-13 10:52:06 -0300

    Mudança do return do Get do relacionamento de Jogo

--- app/Models/Entities/Time.php
+++ app/Models/Entities/Time.php
@@ -72,7 +72,7 @@ class Time extends Model
      * @return string
      */
     public function getJogoAttribute() {
-        return $this->jogoRelationship->nome;
+        return $this->jogoRelationship;
     }
 
     /**
@@ -94,7 +94,7 @@ class Time extends Model
      * @return string
      */
     public function getOrganizacaoAttribute() {
-        return $this->organizacaoRelationship->nome;
+        return $this->organizacaoRelationship;
     }
 
     /**
